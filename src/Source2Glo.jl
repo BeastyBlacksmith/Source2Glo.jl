b2541cc8f42ba60ce4eeae6c33c0fd068bc92c2d
Simon Christ <SimonChrist@gmx.de>
2019-09-01 13:45:55 +0200

    create config_file at init, check for "paths", can register paths

--- src/Source2Glo.jl
+++ src/Source2Glo.jl
@@ -1,6 +1,25 @@
 module Source2Glo
 
+using JSON
 const config_file = "config.json"
 include("configuration.jl")
 
+function __init__()
+    if (!isfile)(config_file)
+        open(config_file, "w") do io
+            JSON.print(io, :paths=>String[])
+        end
+    else
+        local x
+        open( config_file ) do io
+            x = JSON.parse(io)
+        end
+        if haskey(x, "paths")
+            return
+        end
+        open(config_file, "w") do io
+            JSON.print(io, :paths=>String[])
+        end
+    end
+end # function
 end # module
--- src/configuration.jl
+++ src/configuration.jl
@@ -1,5 +1,13 @@
 function register( path; config = config_file )
+    local x
     open( config_file ) do io
-        
+        x = JSON.parse(io)
+    end
+    if path ∈ x["paths"]
+        return
+    end
+    push!(x["paths"], path)
+    open( config_file, "w" ) do io
+        JSON.print( io, x )
     end # do
 end # function
