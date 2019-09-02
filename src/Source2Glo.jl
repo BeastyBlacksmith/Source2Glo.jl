9dadf877a1e8d1c2df1066031571239e0aff0288
Simon Christ <SimonChrist@gmx.de>
2019-09-02 19:49:42 +0200

    fix config file path

--- src/Source2Glo.jl
+++ src/Source2Glo.jl
@@ -5,7 +5,7 @@ using Nettle
 
 const enc = Encryptor("AES256", hex2bytes("603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4"))
 const dec = Decryptor("AES256", hex2bytes("603deb1015ca71be2b73aef0857d77811f352c073b6108d72d9810a30914dff4"))
-const config_file = "config.json"
+const config_file = joinpath(@__DIR__,"../config.json")
 
 include("configuration.jl")
 
@@ -14,6 +14,7 @@ function __init__()
         write_file(config_file, :paths=>String[])
     else
         x = parse_file(config_file)
+        @show x
         if haskey(x, "paths")
             return
         end
